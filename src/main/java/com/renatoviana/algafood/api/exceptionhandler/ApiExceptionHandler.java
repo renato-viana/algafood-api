3effa1ea0ec558464082e3613fe3f21a7a5f8b25
Renato Viana <renatoviana30@gmail.com>
2021-02-20 18:15:31 -0300

    8.26 - Desafio: tratando a exceção NoHandlerFoundException

--- src/main/java/com/renatoviana/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/renatoviana/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.fasterxml.jackson.databind.JsonMappingException.Reference;
@@ -40,6 +41,20 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	// 5. Poderíamos fazer tudo dentro de handleTypeMismatch, mas preferi
 	// separar em outro método
+	
+	@Override
+	protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, 
+	        HttpHeaders headers, HttpStatus status, WebRequest request) {
+	    
+	    ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
+	    String detail = String.format("O recurso %s, que você tentou acessar, é inexistente.", 
+	            ex.getRequestURL());
+	    
+	    Problem problem = createProblemBuilder(status, problemType, detail).build();
+	    
+	    return handleExceptionInternal(ex, problem, headers, status, request);
+	}           
+	
 	@Override
 	protected ResponseEntity<Object> handleTypeMismatch(
 			TypeMismatchException ex, HttpHeaders headers, HttpStatus status,
@@ -138,7 +153,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 			EntidadeNaoEncontradaException ex, WebRequest request) {
 
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
+		ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
 		String detail = ex.getMessage();
 
 		Problem problem = createProblemBuilder(status, problemType, detail)
--- src/main/java/com/renatoviana/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/renatoviana/algafood/api/exceptionhandler/ProblemType.java
@@ -7,8 +7,7 @@ public enum ProblemType {
 
 	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel",
 			"Mensagem incompreensível"),
-	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada",
-			"Entidade não encontrada"),
+	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "recurso não encontrado"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
 	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido");
