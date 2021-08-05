a5574d182f9b58083a4245a04370096441b78a13
Renato Viana <renatoviana30@gmail.com>
2021-08-05 12:23:59 -0300

    11.1 - Analisando e definindo melhor o escopo das transações - @Transactional

--- src/main/java/com/renatoviana/algafood/domain/service/CadastroEstadoService.java
+++ src/main/java/com/renatoviana/algafood/domain/service/CadastroEstadoService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.renatoviana.algafood.domain.exception.EntidadeEmUsoException;
 import com.renatoviana.algafood.domain.exception.EstadoNaoEncontradoException;
@@ -18,10 +19,12 @@ public class CadastroEstadoService {
 	@Autowired
 	EstadoRepository estadoRepository;
 
+	@Transactional
 	public Estado salvar(Estado estado) {
 		return estadoRepository.save(estado);
 	}
 
+	@Transactional
 	public void excluir(Long estadoId) {
 		try {
 
