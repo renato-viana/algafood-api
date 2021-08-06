926fcd95c9f0d765c03ecd590265467ca80e6152
Renato Viana <renatoviana30@gmail.com>
2021-08-06 13:48:25 -0300

    11.7 - Configurando e refatorando o projeto para usar UTC

--- src/main/java/com/renatoviana/algafood/AlgafoodApiApplication.java
+++ src/main/java/com/renatoviana/algafood/AlgafoodApiApplication.java
@@ -1,5 +1,7 @@
 package com.renatoviana.algafood;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +13,8 @@ import com.renatoviana.algafood.infrastructure.repository.CustomJpaRepositoryImp
 public class AlgafoodApiApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		
 		SpringApplication.run(AlgafoodApiApplication.class, args);
 	}
 
--- src/main/java/com/renatoviana/algafood/domain/model/Restaurante.java
+++ src/main/java/com/renatoviana/algafood/domain/model/Restaurante.java
@@ -1,7 +1,7 @@
 package com.renatoviana.algafood.domain.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -65,14 +65,15 @@ public class Restaurante {
 
 	@CreationTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
-	private LocalDateTime dataCadastro;
+	private OffsetDateTime dataCadastro;
 
 	@UpdateTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
-	private LocalDateTime dataAtualizacao;
+	private OffsetDateTime dataAtualizacao;
 
 	@ManyToMany
-	@JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"), inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	@JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"), 
+		inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento = new ArrayList<FormaPagamento>();
 
 	@OneToMany(mappedBy = "restaurante")
