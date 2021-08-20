18c8bfacaa21c33a7b46def1cbec666d52099632
Renato Viana <renatoviana30@gmail.com>
2021-08-20 17:51:06 -0300

    12.11 - Implementando regra de negócio para evitar usuários com e-mails duplicados

--- src/main/java/com/renatoviana/algafood/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/renatoviana/algafood/domain/repository/FormaPagamentoRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.renatoviana.algafood.domain.model.FormaPagamento;
 
-public interface FormaPagamentoRepository
-		extends JpaRepository<FormaPagamento, Long> {
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
 
 }
