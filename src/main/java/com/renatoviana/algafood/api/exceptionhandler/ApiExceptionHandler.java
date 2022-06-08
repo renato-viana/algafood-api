94bc8f093bd6e6846096d60f189c392b6588e835
Renato Viana <renatoviana30@gmail.com>
2022-06-08 12:52:58 -0300

    21.2 - Registrando logs de exceptions não tratadas

--- src/main/java/com/renatoviana/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/renatoviana/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,7 @@ import com.renatoviana.algafood.domain.exception.EntidadeEmUsoException;
 import com.renatoviana.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.renatoviana.algafood.domain.exception.NegocioException;
 
+@Slf4j
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -59,11 +61,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         ProblemType problemType = ProblemType.ERRO_DE_SISTEMA;
         String detail = MSG_ERRO_GENERICA_USUARIO_FINAL;
 
-        // Importante colocar o printStackTrace (pelo menos por enquanto, que não estamos fazendo logging) para mostrar
-        // a stacktrace no console
-        // Se não fizer isso, você não vai ver a stacktrace de exceptions que seriam importantes para você durante,
-        // especialmente na fase de desenvolvimento
-        ex.printStackTrace();
+        log.error(ex.getMessage(), ex);
 
         Problem problem = createProblemBuilder(status, problemType, detail).userMessage(detail).build();
 
