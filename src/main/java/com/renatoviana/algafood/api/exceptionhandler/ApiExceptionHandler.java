f4fe6b208980166a6c12d69201f2f9ea9c2b594b
Renato Viana <renatoviana30@gmail.com>
2022-06-15 15:44:42 -0300

    23.22 - Tratando AccessDeniedException no ExceptionHandler

--- src/main/java/com/renatoviana/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/renatoviana/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.renatoviana.algafood.api.exceptionhandler;
 
-import java.time.OffsetDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.PropertyBindingException;
+import com.renatoviana.algafood.core.validation.ValidacaoException;
+import com.renatoviana.algafood.domain.exception.EntidadeEmUsoException;
+import com.renatoviana.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.renatoviana.algafood.domain.exception.NegocioException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
@@ -14,6 +17,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -26,13 +30,9 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.fasterxml.jackson.databind.JsonMappingException.Reference;
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
-import com.fasterxml.jackson.databind.exc.PropertyBindingException;
-import com.renatoviana.algafood.core.validation.ValidacaoException;
-import com.renatoviana.algafood.domain.exception.EntidadeEmUsoException;
-import com.renatoviana.algafood.domain.exception.EntidadeNaoEncontradaException;
-import com.renatoviana.algafood.domain.exception.NegocioException;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
@@ -72,6 +72,21 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleValidacaoException(ValidacaoException ex, WebRequest request) {
         return handleValidationInternal(ex, ex.getBindingResult(), new HttpHeaders(),
                 HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handleEntidadeNaoEncontrada(AccessDeniedException ex, WebRequest request) {
+
+        HttpStatus status = HttpStatus.FORBIDDEN;
+        ProblemType problemType = ProblemType.ACESSO_NEGADO;
+        String detail = ex.getMessage();
+
+        Problem problem = createProblemBuilder(status, problemType, detail)
+                .userMessage(detail)
+                .userMessage("Você não possui permissão para executar essa operação.")
+                .build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
     @Override
--- src/main/java/com/renatoviana/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/renatoviana/algafood/api/exceptionhandler/ProblemType.java
@@ -5,20 +5,21 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
-	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel",
-			"Mensagem incompreensível"),
-	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "recurso não encontrado"),
-	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
-	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
-	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
-	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
-	DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos");
+    ACESSO_NEGADO("/acesso-negado", "Acesso negado"),
+    MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel",
+            "Mensagem incompreensível"),
+    RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "recurso não encontrado"),
+    ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
+    ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+    PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
+    ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
+    DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos");
 
-	private String title;
-	private String uri;
+    private String title;
+    private String uri;
 
-	private ProblemType(String path, String title) {
-		this.uri = "https://algafood.com.br" + path;
-		this.title = title;
-	}
+    private ProblemType(String path, String title) {
+        this.uri = "https://algafood.com.br" + path;
+        this.title = title;
+    }
 }
